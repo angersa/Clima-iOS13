47abda50fdfaefb723c4de855c25a7a53da4a26b
Annie Angers <annie.angers@umontreal.ca>
2022-01-02 22:33:17 -0500

    Version fonctionnelle en command line

---
+++ Clima/Model/WeatherManager.swift
@@ -0,0 +1,72 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Annie Angers on 2022-01-02.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager {
+    let weatehrURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=df02063e3ec580be49bd5acc20f0e45c"
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatehrURL)&q=\(cityName)"
+        print(urlString)
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        //1. Create a url
+        if let url = URL(string: urlString) {
+            //2. Create a url session
+            let session = URLSession(configuration: .default)
+            //3. Give the session a task
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    self.parseJSON(weatherData: safeData)
+                }
+            }
+            //4. Start the task
+            task.resume()
+        }
+    }
+    
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodeData = try decoder.decode(WeatherData.self, from: weatherData)
+            let id = decodeData.weather[0].id
+            print(getConditionName(weatherId: id))
+        } catch {
+            print(error)
+        }
+    }
+    
+    func getConditionName(weatherId: Int) -> String {
+        switch weatherId {
+        case 200...232:
+            return "cloud.bolt"
+        case 300...321:
+            return "cloud.drizzle"
+        case 500...531:
+            return "cloud.rain"
+        case 600...622:
+            return "cloud.snow"
+        case 701...781:
+            return "cloud.fog"
+        case 800:
+            return "sun.max"
+        case 801...804:
+            return "cloud.bolt"
+        default:
+            return "cloud"
+        }
+
+    }
+}
